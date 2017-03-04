4ab262440a97e13ef09535d5020706a6a65a8486
tomashyam <tomashyam@gmail.com>
2017-03-04 23:17:45 +0200

    finush sort off

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+var shoppingList = angular.module("shoppingList" , [])
+.service('listSyncService' , listSyncService)
+.controller('requierdItems' , requierdItems)
+.controller('boughtItems' , boughtItems);
+
+function listSyncService(){
+  var self = this;
+  self.itesmToBuy = [
+    {"name" : 'cholo', 'amount' : '1'},
+    {"name" : 'milk', 'amount' : '10'},
+    {"name" : 'veggg', 'amount' : '5'},
+  ];
+  self.boughtItems = [];
+
+  self.chekItem = function(item, index){
+      self.boughtItems.push(item);
+      self.itesmToBuy.splice(index ,1);
+  }
+}
+
+requierdItems.$inject = ['$scope' , "listSyncService"]
+function requierdItems($scope , listSyncService){
+  var items = this
+
+  items.itesmToBuy = listSyncService.itesmToBuy;
+  items.markItem = function (item , index){
+    var copyOfItem = {"name" : item.name , "amount" : item.amount}
+    listSyncService.chekItem(copyOfItem , index);
+  }
+
+}
+
+boughtItems.$inject = ['$scope' , "listSyncService"]
+function boughtItems($scope , listSyncService){
+  var items = this;
+  items.boughtItems = listSyncService.boughtItems;
+}
